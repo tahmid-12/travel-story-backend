6f3e333abdabdd6b5d970e283079ba3404ec6cd1
tahmid-12 <mdtahmidchowdhury12@gmail.com>
2025-06-06 01:33:03 +0600

    Search API Created

--- src/routes/travelRoutes.ts
+++ src/routes/travelRoutes.ts
@@ -1,11 +1,12 @@
 import express from 'express';
-import { addTravelStory,getAllTravelStories,editTravelStory,deleteTravelStory,isTraveFavourite } from '../controllers/travelController';
+import { addTravelStory,getAllTravelStories,editTravelStory,deleteTravelStory,isTraveFavourite,searchTravelStories } from '../controllers/travelController';
 import { authenticateToken } from '../utils/validators';
 
 const router = express.Router();
 
 router.post("/add-travel-story", authenticateToken, addTravelStory);
 router.get("/get-all-stories", authenticateToken, getAllTravelStories);
+router.get("/search-travel-stories", authenticateToken, searchTravelStories);
 router.put("/edit-travel-story/:storyId",authenticateToken,editTravelStory);
 router.put("/is-travel-favourite/:storyId",authenticateToken,isTraveFavourite);
 router.delete("/delete-travel-story/:storyId",authenticateToken,deleteTravelStory);
