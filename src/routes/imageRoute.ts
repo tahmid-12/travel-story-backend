673dba3ae5d23795640e520e39fedbca621072b0
tahmid-12 <mdtahmidchowdhury12@gmail.com>
2025-06-05 20:20:22 +0600

    Delete Image API Crated

--- src/routes/imageRoute.ts
+++ src/routes/imageRoute.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 import { upload } from '../utils/multer';
 import { authenticateToken } from '../utils/validators';
-import { imageUpload } from '../controllers/imageController';
+import { imageUpload, imageDelete } from '../controllers/imageController';
 
 const router = express.Router();
 
 router.post('/upload-image', authenticateToken, upload.single('image'), imageUpload);
+router.delete('/delete-image', authenticateToken, imageDelete);
 
 export default router;
